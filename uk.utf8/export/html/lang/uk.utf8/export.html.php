785079cd6fce9b2bf2d8f588e37ad7766da657df
jshvets <47864584+jshvets@users.noreply.github.com>
2019-02-22 13:36:55 +0200

    Add files via upload

---
+++ uk.utf8/export/html/lang/uk.utf8/export.html.php
@@ -0,0 +1,12 @@
+<?php
+
+defined('INTERNAL') || die();
+
+$string['buildingindexpage'] = 'Création de la page d\'index';
+$string['copyingextrafiles'] = 'Duplication des fichiers supplémentaires';
+$string['description'] = 'Crée un site web autonome avec votre portfolio. Vous ne pouvez pas le réimporter, mais il est lisible à l\'aide d\'un navigateur web.';
+$string['duplicatepagetitle'] = 'L\'exportation a échoué car deux pages portent le même titre. Veuillez vous assurer à ce que les titres de pages soient uniques.';
+$string['exportingdatafor'] = 'Exportation des données pour %s';
+$string['preparing'] = 'Préparation de %s';
+$string['title'] = 'Site web HTML autonome';
+$string['usersportfolio'] = 'Portfolio de %s';
---
+++ uk.utf8/export/leap/lang/uk.utf8/export.leap.php
@@ -0,0 +1,6 @@
+<?php
+
+defined('INTERNAL') || die();
+
+$string['description1'] = 'Ceci crée un fichier d\'exportation au <a href="https://web.archive.org/web/20140912151415/http://wiki.leapspecs.org/2A/specification">format standard Leap2A </a>. Vous pouvez utiliser ce fichier ultérieurement pour importer vos données dans <a href="https://wiki.mahara.org/wiki/Developer_Area/Import//Export/Interoperability">un autre système compatible Leap2A</a>; la lecture du fichier Leap2A est quelque peu compliquée pour un être humain.';
+$string['title'] = 'Leap2A';
