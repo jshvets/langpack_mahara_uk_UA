d964362e6a3dee30d52ed6b72145a1bb21a6b4a5
jshvets <47864584+jshvets@users.noreply.github.com>
2019-02-27 21:14:26 +0200

    Add files via upload

---
+++ uk.utf8/lang/uk.utf8/export.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ *
+ * @package    mahara
+ * @subpackage lang
+ * @author     Catalyst IT Ltd
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
+ * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
+ *
+ */
+
+defined('INTERNAL') || die();
+
+$string ['allmydata'] = 'Всі мої дані';
+$string ['chooseanexportformat'] = 'Виберіть формат експорту';
+$string ['clicktopreview'] = 'Натисніть для перегляду';
+$string ['collectionstoexport'] = 'Колекції для експорту';
+$string ['creatingzipfile'] = 'Створення zip-файлу';
+$string ['Done'] = 'Виконано';
+$string ['Export'] = 'Експорт';
+$string ['clickheretodownload'] = 'Натисніть тут, щоб завантажити його';
+$string ['continue'] = 'Продовжити';
+$string ['exportgeneratedsuccessfully'] = 'Експортування успішно виконано. %s Натисніть тут, щоб завантажити його %s ';
+$string ['exportgeneratedsuccessfully1'] = 'Успішно експортовано.';
+$string ['exportgeneratedwitherrors'] = 'Експортувати з деякими помилками.';
+$string ['exportartefactplugindata'] = 'Експорт даних модуля артефактів';
+$string ['exportartefacts'] = 'Експортування артефактів';
+$string ['exportartefactsprogress'] = 'Експортування артефактів: %s/%s';
+$string ['exportsfooter'] = 'Експортування нижнього колонтитула';
+$string ['exportsviews'] = 'Експортування сторінок';
+$string ['exportscollections'] = 'Експортування колекцій';
+$string ['exportviewsprogress'] = 'Експортування сторінок: %s/%s';
+$string ['exportportfoliodescription'] = 'Цей інструмент експортує всю інформацію про ваше портфоліо і сторінки. Він не експортує налаштувань сайту або будь-якого вмісту, завантаженого або створеного в групах. ';
+$string ['exportyourportfolio'] = 'Експортувати ваше портфоліо';
+$string ['generateexport'] = 'Згенерувати експорт';
+$string ['noexportpluginsenabled'] = 'Адміністратор сайту не ввімкнув додатки для експорту, тому ви не можете використовувати цю функцію';
+$string ['justsomecollections'] = 'Просто деякі з моїх колекцій';
+$string ['justsomeviews'] = 'Просто деякі мої сторінки';
+$string ['includecomments'] = 'Включити коментарі';
+$string ['includecommentsdescription'] = 'Всі коментарі будуть включені в експорт HTML.';
+$string ['nonexistentfile'] = 'Намагався додати неіснуючий файл «%s».';
+$string ['nonexistentprofileicon'] = 'Намагався додати неіснуючу піктограму профайлу «%s».';
+$string ['nonexistentresizedprofileicon'] = 'Намагався додати неіснуючу піктограму профайлу «%s».';
+$string ['unabletocopyartefact'] = 'Неможливо скопіювати артефактний файл «%s».';
+$string ['unabletocopyprofileicon'] = 'Неможливо скопіювати піктограму профайлу «%s».';
+$string ['unabletocopyresizedprofileicon'] = 'Не вдалося скопіювати піктограму профайлу «%s».';
+$string ['couldnotcreatedirectory'] = 'Не вдалося створити каталог «%s»';
+$string ['couldnotcreatestaticdirectory'] = 'Неможливо створити статичний каталог «%s»';
+$string ['couldnotcopystaticfile'] = 'Не вдалося скопіювати статичний файл «%s».';
+$string ['couldnotcopyattachment'] = 'Не вдалося скопіювати вкладення «%s».';
+$string ['couldnotcopyfilesfromto'] = 'Не вдалося скопіювати файли з каталогу «%s» до «%s».';
+$string ['couldnotwriteLEAPdata'] = 'Не вдалося записати дані Leap2A у файл.';
+$string ['pleasewaitwhileyourexportisbeinggenerated'] = 'Будь ласка, зачекайте поки буде створено ваш експорт ...';
+$string ['reverseselection'] = 'Зворотній вибір';
+$string ['selectall'] = 'Вибрати всі';
+$string ['setupcomplete'] = 'Встановлення завершено';
+$string ['Starting'] = 'Початок';
+$string ['unabletoexportportfoliousingoptions'] = 'Неможливо експортувати портфоліо за допомогою вибраних опцій';
+$string ['unabletogenerateexport'] = 'Неможливо згенерувати експорт';
+$string ['viewstoexport'] = 'Сторінки для експорту';
+$string ['whatdoyouwanttoexport'] = 'Що потрібно експортувати?';
+$string ['writingfiles'] = 'Запис файлів';
+$string ['youarehere'] = 'Ви тут';
+$string ['youmustselectatleastonecollectiontoexport'] = 'Ви повинні вибрати принаймні одну колекцію для експорту';
+$string ['youmustselectatleastoneviewtoexport'] = 'Ви повинні вибрати хоча б одну сторінку для експорту';
+$string ['zipnotinstalled'] = 'У вашій системі немає команди ZIP. Щоб увімкнути цю функцію, установіть ZIP. ';
+$string ['addedleap2atoexportqueuecollections'] = 'Додано деякі з ваших колекцій до черги експорту.';
+$string ['addedleap2atoexportqueueviews'] = 'Додано деякі сторінки до черги експорту.';
+$string ['addedleap2atoexportqueueall'] = 'Додано всі ваші дані до черги експорту.';
+$string ['exportqueuenotempty'] = 'Елементи в черзі експорту для цього користувача. Зачекайте, поки вони не будуть заархівовані';
+$string ['requeue'] = 'Оновити чергу';
+
+// Експорт помилок черги
+$string ['unabletogenerateexport'] = 'Недостатня інформація';
+$string ['unabletoexportportfoliousingoptionsadmin'] = 'Елемент не є сторінкою або об\'єктом колекції';
+$string ['exportzipfileerror'] = 'Помилка генерації файлу ZIP: %s';
+$string ['submissiondirnotwritable'] = 'Неможливо записати до каталогу архіву подання: %s';
+$string ['exportarchivesavefailed'] = 'Неможливо зберегти інформацію експорту архіву до бази даних';
+$string ['archivedsubmissionfailed'] = 'Неможливо зберегти архівовану інформацію про подання до бази даних';
+$string ['submissionreleasefailed'] = 'Не вдалося представити подання після архівування';
+$string ['deleteexportqueueitems'] = 'Не вдалося видалити елементи черги експорту з таблиці елементів бази даних.';
+$string ['deleteexportqueuerow'] = 'Не вдалося видалити елементи з черги експорту таблиці бази даних. ';
+$string ['exportqueueerrorsadminsubject'] = 'Помилка запуску черги експорту';
+$string ['exportqueueerrorsadminmessage'] = 'Не вдається експортувати рядок «%s» через: %s';
